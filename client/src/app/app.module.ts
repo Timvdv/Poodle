766e3c4d7f8997ea740f63787c007c2559eac5a2
Tim van de Vathorst <tim@vandevathorst.nl>
2017-05-08 10:07:40 +0200

    Add: initial components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DrawComponent } from './draw/draw.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

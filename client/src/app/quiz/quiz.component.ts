6a6a9cb1b15ccc66ef0cb5842b1400af89a2e488
maartenesser <maartenjr.esser@gmail.com>
2017-05-31 15:58:29 +0200

    Quiz component uitgewerkt

--- client/src/app/quiz/quiz.component.ts
+++ client/src/app/quiz/quiz.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+    selector: 'app-quiz',
+    templateUrl: './quiz.component.html',
+    styleUrls: ['./quiz.component.css']
 })
+
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+    clicked(event) {
+        var target = event.target || event.srcElement || event.currentTarget;
+        var Attr = target.attributes.data;
+        var string = Attr.nodeValue;
 
-  ngOnInit() {
-  }
+        if (string == 'true') {
+            alert("Great! That was the right answer");
+        } else {
+            alert("wrong answer!");
+        }
+    }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+
+    }
 
 }
+
+
+
+
+
+
+
+

05df04e7cfdceb6efe64c8a672ba91a7a1ad9877
maartenesser <maartenjr.esser@gmail.com>
2017-06-05 15:49:36 +0200

    Add: Display components

--- server/src/Connectors/SocketConnection.js
+++ server/src/Connectors/SocketConnection.js
@@ -20,8 +20,8 @@ module.exports = function SocketConnection(adapter) {
         });
 
         socket.on('getImages', function (gameId) {
-            var doodles = adapter.getGameDoodles(gameId);
-                socket.emit('setImages', doodles);
+            // var doodles = adapter.getGameDoodles(gameId);
+            //     socket.emit('setImages', doodles);
         });
 
         socket.on('updateImages', function(image) {

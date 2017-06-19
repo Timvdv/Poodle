126044f1e02476e3a474fef12e951d384a5624d8
Oteken <Otekenpekince@hotmail.com>
2017-06-19 17:20:36 +0200

    Added version checking when getting an express server.

--- server/src/ExpressAdapter.js
+++ server/src/ExpressAdapter.js
@@ -2,12 +2,21 @@
  * Created by oteken on 6/19/2017.
  */
 var express = require('express');
+var packageJson = require('../package.json');
 
 module.exports = function ExpressAdapter(){
 
     this.getExpressServer = function(){
-        var expressServer = express();
+        var expressServer;
+        var expressVersion = getExpressVersion();
+        if(expressVersion == "^4.15.2") {
+            expressServer = express();
+        }
         return expressServer;
+    }
+
+    function getExpressVersion(){
+        return packageJson.dependencies.express;
     }
 }
 
